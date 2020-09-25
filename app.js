6c61bb109d73443b39e585fdf37a6b28d74a226b
heretic <71768431+jhinysoh@users.noreply.github.com>
2020-09-25 12:52:28 +0900

    aa

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req,res) {
-  res.send('Hello Paper!');
+  res.send('Hello Paper!!!!');
 });
 
 app.listen(3000, function(){
