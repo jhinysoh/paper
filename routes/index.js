63093ec4c6769e01a33625449270ea53644f4f28
unknown <jhinysoh@gmail.com>
2020-09-27 12:17:43 +0900

    temp

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.pug');
+  res.render('../public/index.pug');
 });
 
 module.exports = router;
