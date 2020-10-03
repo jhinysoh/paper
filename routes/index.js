1ee6ea73a0a5c973732a9d791383d4522e52cba8
unknown <jhinysoh@gmail.com>
2020-10-04 02:24:27 +0900

    session

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var router = express.Router();
+
 
 //- mongoDB 연결
 var mongoose = require('mongoose');
@@ -10,15 +13,42 @@ db.once('open', () => {
 	console.log("DB connected");
 });
 
+
+//- 세션 사용
+router.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+	store: new FileStore()
+}));
+
+
+//- 로그인
+var authData = { email: 'jhinysoh@gmail.com', password:'trust', nickname: 'jk' }
+router.post('/auth', function(req, res){
+	var post = req.body;
+	var email = post.email;
+	var password = post.pw;
+	if(email===authData.email && password===authData.password){
+		res.redirect ('paper');
+	} else {  }
+});
+
 //- GET home page
 router.get('/', function(req, res, next) {
+	console.log(req.session);
+	if(req.session.num === undefined) {
+		req.session.num = 1;
+	} else { req.session.num++;}
   res.render('index');
 });
+
 
 //- GET 회원가입
 router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
+
 
 //- GET 페이퍼
 router.get('/paper', function(req, res, next) {
