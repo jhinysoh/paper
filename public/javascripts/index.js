0ee436debe218eee74c44df38f5e2ed82fc1d00c
jhinysoh <jhinysoh@gmail.com>
2020-11-08 18:29:27 +0000

    paper

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,15 @@
 window.addEventListener('DOMContentLoaded',function(){
 
     // 회원가입, 병원등록 페이지 토글
-    const signUpNav = document.getElementsByClassName('signup-nav');
-    for(var i=0; i<signUpNav.length; i++){ signUpNav[i].addEventListener('click',signUpFormSwitch) }
+    let signUpNav = document.querySelectorAll('.signUpNav');
+    for(let i=0; i<signUpNav.length; i++){ signUpNav[i].addEventListener('click',signUpFormSwitch) }
 
 });
 
-    function signUpFormSwitch(){
-        document.getElementsByClassName("signup-nav").classList.toggle("signup-nav1");
-    	document.getElementsByClassName("signUpForm").classList.toggle("formHide")
-    }
-//const signUpNav = document.getElementsByClassName('signup-nav');
-//function signUpFormSwitch(){
-//    const signUpNav = document.getElementsByClassName('signup-nav');
-//    for(var i=0; i<signUpNav.length; i++){ signUpNav[i].classList.toggle('signup-nav1') }
+function signUpFormSwitch(){
+    let signUpNav = document.querySelectorAll('.signUpNav');
+    let signUpForm = document.querySelectorAll('form');
+    for(let i=0; i<signUpNav.length; i++){ signUpNav[i].classList.toggle('signUpNav1') }
+    for(let i=0; i<signUpForm.length; i++){ signUpForm[i].classList.toggle('formHide') }
+}
 
-//	document.getElementById("userSignUp").classList.toggle("signup-nav1");
-//	document.getElementById("hospitalSignUp").classList.toggle("signup-nav1");
-//	document.getElementById("userSignUpForm").classList.toggle("formHide");
-//	document.getElementById("hospitalSignUpForm").classList.toggle("formHide")
-//}
-
-
-/*
-document.getElementById("hospitalSignUp").addEventListener("click",alert());
-
-
-
-*/
