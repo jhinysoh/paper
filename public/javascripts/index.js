a4422f2e481c6e3c3f6eb7b291928fb54ebfc127
unknown <jhinysoh@gmail.com>
2020-11-16 00:30:12 +0900

    signup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,9 +6,9 @@ window.addEventListener('DOMContentLoaded',function(){
       goSignUp.addEventListener('click',function(){
         document.querySelectorAll('.main').forEach( (main) => {
           main.classList.toggle('hide');
-          main.querySelectorAll('input[type=email],input[type=password]')
-            .forEach( (input) => { input.value = '' });
-          main.querySelector('input').focus()
+          main.querySelectorAll('input:not([type=submit]):not([type=button])')
+            .forEach( (item) => { item.value='' });
+          main.querySelector('form:not(.hide) input').focus()
         })
       })
     });
@@ -20,20 +20,55 @@ window.addEventListener('DOMContentLoaded',function(){
       item.addEventListener('click',function(){
         signUpNav.forEach( (nav) => { nav.classList.toggle('signUpNavDark') });
         signUpForm.forEach( (nav) => { nav.classList.toggle('hide') });
-        signUpForm[i].querySelectorAll('input[type=email],input[type=text],input[type=password],input[type=number]' )
-          .forEach( (input) => { input.value = '' });
+        signUpForm[i].querySelectorAll('input:not([type=submit]):not([type=button])')
+          .forEach( (input) => {
+            input.value = '';
+            input.style.backgroundColor = ''
+          });
         signUpForm[i].querySelector('input').focus()
       });
     });
 
+    // 이메일 확인
+    const regId = /^[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/;
+    const email = document.querySelectorAll('input[type=email]');
+    email.forEach((item, i) => {
+      item.addEventListener('blur',function(){
+        if( regId.test(this.value)===false && this.value!=='' ){
+          this.value = '';
+          this.style.backgroundColor = '#FADBD8';
+          this.placeholder = '이메일형식으로 입력해주세요'
+        } else{
+          this.style.backgroundColor = '';
+          this.placeholder = '이메일'
+        }
+      })
+    });
+
     // 비밀번호 확인
-    const reg = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[,./<>?!@#$%^&*()_+-=]).{8,}$/;
+    const regPw = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,20}$/
     const password = document.querySelectorAll('.password');
     password[0].addEventListener('blur',function(){
-      if( reg.test(password[0].value) === false ){ alert('비밀번호는 영소문자, 숫자, 특수문자를 포함하여 8자 이상이어야합니다.') }
+      if( regPw.test(this.value)===false && this.value!=='' ){
+        this.value = '';
+        this.style.backgroundColor = '#FADBD8';
+        this.placeholder ='영문자/숫자/특수문자 포함 8자이상';
+        this.focus()
+      } else{
+        this.style.backgroundColor = '';
+        this.placeholder = '비밀번호'
+      }
     });
     password[1].addEventListener('blur',function(){
-      if( password[0].value !== password[1].value ){ alert('비밀번호가 일치하지 않습니다') }
+      if( password[0].value!==this.value && this.value!='' ){
+        this.value = '';
+        this.style.backgroundColor = '#FADBD8';
+        this.placeholder ='비밀번호가 일치하지않습니다';
+        this.focus()
+        } else{
+          this.style.backgroundColor = '';
+          this.placeholder = '비밀번호확인'
+        }
     });
 
 
